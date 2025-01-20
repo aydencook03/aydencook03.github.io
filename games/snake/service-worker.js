a314ab20ff75a1a60b408d7f045fbcf4fd89b706
Ayden Cook <cookayden2003@gmail.com>
2025-01-19 21:08:59 -0500

    new high score counter for snake

--- games/snake/service-worker.js
+++ games/snake/service-worker.js
@@ -1,23 +1,41 @@
-self.addEventListener('install', function (event) {
-    // Perform install steps
+const CACHE_NAME = 'my-game-cache-v8';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json',
+    './service-worker.js',
+    './icons/icon-192.png',
+    './icons/icon-512.png',
+    './privacy-policy.html'
+];
+
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('my-game-cache-v6').then(function (cache) {
-            return cache.addAll([
-                './',
-                './index.html',
-            ]);
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
         })
     );
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then(function (response) {
-            // Cache hit - return response
-            if (response) {
-                return response;
-            }
-            return fetch(event.request);
+        caches.match(event.request).then((response) => {
+            // Serve cached file if it exists, else fetch from network
+            return response || fetch(event.request);
         })
     );
 });
